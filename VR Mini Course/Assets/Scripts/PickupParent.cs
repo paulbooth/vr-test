d4c6a3904d24cd1a52513a72c57104e6d4aa46f8
Paul Booth <thepaulbooth@gmail.com>
2016-05-22 19:28:44 -0700

    Pickup

---
+++ VR Mini Course/Assets/Scripts/PickupParent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedObj;
+	SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Awake () {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("holding down the trigger");
+		}
+	}
+
+	void OnTriggerStay (Collider col)
+	{
+		if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			col.attachedRigidbody.isKinematic = true;
+			col.gameObject.transform.SetParent (gameObject.transform);
+		}
+		if (device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			col.gameObject.transform.SetParent (null);
+			col.attachedRigidbody.isKinematic = false;
+		}
+	}
+}
