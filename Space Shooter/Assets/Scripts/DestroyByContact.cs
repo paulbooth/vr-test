fa745d682454215642c506151cc275189c103255
Paul Booth <thepaulbooth@gmail.com>
2016-05-24 21:58:07 -0700

    Space Shooter.

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.CompareTag("Boundary")) {
+			return;
+		}
+		Destroy (col.gameObject);
+		Destroy (gameObject); 
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (col.CompareTag ("Player")) {
+			Instantiate (playerExplosion, col.transform.position, col.transform.rotation);
+		}
+	}
+}
---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValue;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+				Instantiate (hazard, spawnPosition, Quaternion.identity);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update () {
+	
+	}
+}
