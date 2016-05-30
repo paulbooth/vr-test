1b7da631943422fd4ac79cc18d82fff9422089e7
Paul Booth <thephantompaulbooth@gmail.com>
2016-05-29 22:54:41 -0700

    Construction time.

---
+++ Spawnsor/Assets/Constructable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using NewtonVR;
+
+public class Constructable : NVRInteractableItem
+{
+	public float breakForce = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void UseButtonDown()
+	{
+		base.UseButtonDown ();
+		NVRPlayer player = AttachedHand.GetComponentInParent<NVRPlayer> ();
+		NVRInteractable left = player.LeftHand.CurrentlyInteracting;
+		NVRInteractable right = player.RightHand.CurrentlyInteracting;
+		Constructable leftCons = left.gameObject.GetComponent<Constructable> ();
+		Constructable rightCons = right.gameObject.GetComponent<Constructable> ();
+		if (leftCons && rightCons && player.LeftHand.UseButtonPressed && player.RightHand.UseButtonPressed) {
+			Debug.Log ("MAKING CONNECTION");
+			FixedJoint f = left.gameObject.AddComponent<FixedJoint> ();
+			f.breakForce = breakForce;
+			f.connectedBody = right.gameObject.GetComponent<Rigidbody> ();
+		}
+	}
+}
