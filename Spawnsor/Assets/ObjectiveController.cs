e8d9bc7d2b89f03eb5f143ef35511efce6edf6ca
Paul Booth <thephantompaulbooth@gmail.com>
2016-05-30 14:18:19 -0700

    Add build objective

---
+++ Spawnsor/Assets/ObjectiveController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveController : MonoBehaviour {
+
+	public SpawnController spawnController;
+	public float completeTime = 3f;
+
+	private float allCompleteStartTime = 0f;
+	private bool objectiveMet = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (objectiveMet) {
+			return;
+		}
+		bool allComplete = AllRequirementsMet ();
+		if (allComplete) {
+			if (allCompleteStartTime > 0f) {
+				if (Time.time >= allCompleteStartTime + completeTime) {
+					objectiveMet = true;
+					spawnController.SpawnGun (Color.green, 10, 10, 0);
+				}
+			} else {
+				allCompleteStartTime = Time.time;
+			}
+		} else {
+			allCompleteStartTime = 0f;
+		}
+	}
+
+	bool AllRequirementsMet()
+	{
+		bool allComplete = true;
+		foreach (ObjectDetector o in GetComponentsInChildren<ObjectDetector>()) {
+			if (!o.areRequirementsMet ()) {
+				allComplete = false;
+				break;
+			}
+		}
+		return allComplete;
+	}
+}
---
+++ Spawnsor/Assets/Scripts/ObjectDetector.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDetector : MonoBehaviour {
+	public bool shouldCollide = true;
+	public bool isColliding = false;
+
+	private int numColliding = 0;
+
+	void Start () {
+		updateState ();
+	}
+
+	public bool areRequirementsMet() {
+		return shouldCollide == isColliding;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (eligibleForCollision(col)) {
+			isColliding = true;
+			numColliding++;
+			updateState ();
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (eligibleForCollision(col)) {
+			numColliding--;
+			if (numColliding <= 0) {
+				isColliding = false;
+				updateState ();
+			}
+		}
+	}
+
+	bool eligibleForCollision(Collider col) {
+		return col.gameObject.GetComponent<Constructable> ();
+	}
+
+	void updateState() {
+		bool requirementsMet = areRequirementsMet ();
+		MeshRenderer renderer = GetComponent<MeshRenderer> ();
+		Color color = renderer.material.color;
+		if (requirementsMet) {
+			color.r = 0;
+			color.g = 255;
+		} else {
+			color.r = 255;
+			color.g = 0;
+		}
+		renderer.material.color = color;
+
+		Light l = GetComponent<Light> ();
+		if (l) {
+			Color lightColor = l.color;
+			lightColor.r = requirementsMet ? 0 : 1;
+			lightColor.g = requirementsMet ? 1 : 0;
+			l.color = lightColor;
+		}
+	}
+}
